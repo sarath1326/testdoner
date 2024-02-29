aa9ee3ddfc733304ebc7e84f310592dedd6a78f3
sarath <sarathsarath93366@gmail.com>
2024-02-29 23:36:13 +0530

    test 6

--- server/Routers/authRout.js
+++ server/Routers/authRout.js
@@ -10,15 +10,15 @@
 
 const express=require("express");
 const router=express();
-// const control=require("../Control/authContro")
+const control=require("../Control/authContro")
 const authmid=require("../Middleware/userauthmid")
 
 
 
-// router.get("/connecting",control.connecting)
-// router.post("/signup",  control.signup_data);
-// router.post("/login",control.login_user);
-// router.get("/account",authmid.user_auth_verify,control.user_account_data );
+router.get("/connecting",control.connecting)
+router.post("/signup",  control.signup_data);
+router.post("/login",control.login_user);
+router.get("/account",authmid.user_auth_verify,control.user_account_data );
 
 
 
