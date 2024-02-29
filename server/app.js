de04b7fb460c2a6aa5a5f4fe67ff66b7f2f03708
sarath <sarathsarath93366@gmail.com>
2024-02-29 22:58:10 +0530

    test 1

--- server/app.js
+++ server/app.js
@@ -5,20 +5,20 @@
 const express = require("express");
 const app = express()
 const DB = require("./Database/connect")
-const authRoute = require("./Routers/authRout")
-const searchRout=require("./Routers/searchRout")
-const bbauth=require("./Routers/bloodbankRout")
+// const authRoute = require("./Routers/authRout")
+// const searchRout=require("./Routers/searchRout")
+// const bbauth=require("./Routers/bloodbankRout")
 
 const cors = require("cors")
-const bodyparser= require("body-parser")
-const cookieparser=require("cookie-parser");
+const bodyparser = require("body-parser")
+const cookieparser = require("cookie-parser");
 require('dotenv').config();
-const PORT=process.env.PORT || 3001
+const PORT = process.env.PORT || 3001
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.urlencoded({ extended: true }))
 app.use(cookieparser());
 
 
@@ -27,31 +27,35 @@ app.use(cookieparser());
 
 
 app.use(cors({
-      origin : ["http://localhost:3000",],
+      origin: ["http://localhost:3000",],
       methods: ["GET", "POST"],
       credentials: true
-  }));
+}));
 
 
 
 DB.DBConnect();  // Database connecting func
 
 
-app.use("/auth", authRoute); //auth/register
+// app.use("/auth", authRoute); //auth/register
 
-app.use("/search",searchRout) 
-
-
-app.use("/bloodbank",bbauth)  // blood bank auth  
+// app.use("/search",searchRout) 
 
 
+// app.use("/bloodbank",bbauth)  // blood bank auth  
+
+
+app.get("/home", (req, res) => {
+
+      res.json({ msg: "hiiii" })
+
+})
 
 
 
+app.listen(PORT, () => {
 
-app.listen(PORT,()=>{
-
-      console.log( `server started ${PORT}`)
+      console.log(`server started ${PORT}`)
 })
 
 
